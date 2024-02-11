c646aa57506300a17f73b255b001364b943de13c
Peter A. <ink.splatters@pm.me>
2024-02-11 13:29:47 +0100

    fixed issue

--- flake.nix
+++ flake.nix
@@ -51,17 +51,20 @@
 
         formatter = pkgs.nixfmt;
 
-        devShells.default = mkShell.override { inherit stdenv; } {
-          inherit (self.packages.${system}.default) CFLAGS nativeBuildInputs;
+        devShells = {
+          default = mkShell.override { inherit stdenv; } {
+            inherit (self.packages.${system}.default) CFLAGS nativeBuildInputs;
 
-          shellHook = self.checks.${system}.pre-commit-check.shellHook + ''
-            export PS1="\n\[\033[01;36m\]‹⊂› \\$ \[\033[00m\]"
-            echo -e "\nto install pre-commit hooks:\n\x1b[1;37mnix develop .#install-hooks\x1b[00m"
-          '';
-        };
+            shellHook = self.checks.${system}.pre-commit-check.shellHook + ''
+              export PS1="\n\[\033[01;36m\]‹⊂› \\$ \[\033[00m\]"
+              echo -e "\nto install pre-commit hooks:\n\x1b[1;37mnix develop .#install-hooks\x1b[00m"
+            '';
+          };
 
-        install-hooks = mkShell {
-          inherit (self.checks.${system}.pre-commit-check) shellHook;
+          install-hooks = mkShell {
+            inherit (self.checks.${system}.pre-commit-check) shellHook;
+          };
+
         };
 
         packages.default = stdenv.mkDerivation {
